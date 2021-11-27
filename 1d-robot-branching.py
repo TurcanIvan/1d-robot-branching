5de5721a53d4495a30dac156b67852dbf5174772
TurcanIvan <68420218+TurcanIvan@users.noreply.github.com>
2021-11-27 07:38:45 +0200

    Example

---
+++ 1d-robot-branching.py
@@ -0,0 +1,90 @@
+# ---------- R --------->
+# 0 
+
+batteryCharge = 70   #%
+ratePerStep   = 10   # 10%/m
+
+steps = 0
+
+# COMPLEXITY
+# DRY - Don't Repeat Yourself 
+
+############# one step ############################
+steps         += 1           # increment
+batteryCharge -= ratePerStep # decrement
+print("steps", steps,"battery:",batteryCharge, "%")
+if batteryCharge <= 10:
+    print("WAITING!!! need to charge")
+############# one step ############################
+
+############# one step ############################
+steps         += 1           # increment
+batteryCharge -= ratePerStep # decrement
+print("steps", steps,"battery:",batteryCharge, "%")
+if batteryCharge <= 10:
+    print("WAITING!!! need to charge")
+############# one step ############################
+
+############# one step ############################
+steps         += 1           # increment
+batteryCharge -= ratePerStep # decrement
+print("steps", steps,"battery:",batteryCharge, "%")
+if batteryCharge <= 10:
+    print("WAITING!!! need to charge")
+############# one step ############################
+
+############# one step ############################
+steps         += 1           # increment
+batteryCharge -= ratePerStep # decrement
+print("steps", steps,"battery:",batteryCharge, "%")
+if batteryCharge <= 10:
+    print("WAITING!!! need to charge")
+############# one step ############################
+
+############# one step ############################
+steps         += 1           # increment
+batteryCharge -= ratePerStep # decrement
+print("steps", steps,"battery:",batteryCharge, "%")
+if batteryCharge <= 10:
+    print("WAITING!!! need to charge")
+############# one step ############################
+
+############# one step ############################
+steps         += 1           # increment
+batteryCharge -= ratePerStep # decrement
+print("steps", steps,"battery:",batteryCharge, "%")
+if batteryCharge <= 10:
+    print("WAITING!!! need to charge")
+############# one step ############################
+
+############# one step ############################
+steps         += 1           # increment
+batteryCharge -= ratePerStep # decrement
+print("steps", steps,"battery:",batteryCharge, "%")
+if batteryCharge <= 10:
+    print("WAITING!!! need to charge")
+############# one step ############################
+
+############# one step ############################
+steps         += 1           # increment
+batteryCharge -= ratePerStep # decrement
+print("steps", steps,"battery:",batteryCharge, "%")
+if batteryCharge <= 10:
+    print("WAITING!!! need to charge")
+############# one step ############################
+
+############# one step ############################
+steps         += 1           # increment
+batteryCharge -= ratePerStep # decrement
+print("steps", steps,"battery:",batteryCharge, "%")
+if batteryCharge <= 10:
+    print("WAITING!!! need to charge")
+############# one step ############################
+
+############# one step ############################
+steps         += 1           # increment
+batteryCharge -= ratePerStep # decrement
+print("steps", steps,"battery:",batteryCharge, "%")
+if batteryCharge <= 10:
+    print("WAITING!!! need to charge")
+############# one step ############################
